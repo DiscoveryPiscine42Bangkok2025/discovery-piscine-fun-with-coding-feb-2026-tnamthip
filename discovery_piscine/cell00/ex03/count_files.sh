68cead45a2629c9df226c26bb65a8940f15db5df
tnamthip <65010363@kmitl.ac.th>
2026-02-03 20:46:05 +0000

    Upload Discovery PC

---
+++ discovery_piscine/cell00/ex03/count_files.sh
@@ -0,0 +1,4 @@
+ls | wc -l
+#@tnamthip ➜ .../discovery-piscine-fun-with-coding-feb-2026-tnamthip/discovery_piscine/cell00/ex03 (main) $ echo "ls | wc -l" > count_files.sh
+#@tnamthip ➜ .../discovery-piscine-fun-with-coding-feb-2026-tnamthip/discovery_piscine/cell00/ex03 (main) $ chmod +x count_files.sh
+#@tnamthip ➜ .../discovery-piscine-fun-with-coding-feb-2026-tnamthip/discovery_piscine/cell00/ex03 (main) $ ./count_files.sh | cat -e
