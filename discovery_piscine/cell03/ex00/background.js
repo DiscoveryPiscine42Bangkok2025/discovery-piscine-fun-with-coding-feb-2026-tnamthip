026f70b133f3e5192071558a05c420de6a5fc04c
tnamthip <65010363@kmitl.ac.th>
2026-02-04 14:56:04 +0000

    Complete

--- discovery_piscine/cell03/ex00/background.js
+++ discovery_piscine/cell03/ex00/background.js
@@ -1,6 +1,4 @@
 function changeColor() {
-    // สร้างตัวเลขสุ่มสำหรับรหัสสี Hex
     const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-    // เปลี่ยนสีพื้นหลังของ body
     document.body.style.backgroundColor = randomColor;
 }
