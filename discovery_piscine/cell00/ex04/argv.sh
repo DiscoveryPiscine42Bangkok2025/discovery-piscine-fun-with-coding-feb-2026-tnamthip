b89eac6f20c92a81c1a16f931616216f014435e9
tnamthip <65010363@kmitl.ac.th>
2026-02-04 07:55:55 +0000

    Complete

--- discovery_piscine/cell00/ex04/argv.sh
+++ discovery_piscine/cell00/ex04/argv.sh
@@ -7,3 +7,18 @@ else
         echo "$arg"
     done
 fi
+
+#$>./argv.sh
+#No arguments supplied
+#$>./argv.sh 42
+#42
+#$>./argv.sh 2 40 42
+#2
+#40
+#42
+#$>./argv.sh Hello World ! 42
+#Hello
+#World
+#!
+#42
+#$>
